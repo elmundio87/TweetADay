52dd19ec87ed3c69b4da7140278d5555038bff06
Edmund Dipple <elmundio1987@gmail.com>
2011-05-01 23:45:30 +0100

    first commit

---
+++ Form1.Designer.vb
@@ -0,0 +1,160 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.edtUsername = New System.Windows.Forms.TextBox
+        Me.Label1 = New System.Windows.Forms.Label
+        Me.DateTimePicker1 = New System.Windows.Forms.DateTimePicker
+        Me.Label2 = New System.Windows.Forms.Label
+        Me.btnSearch = New System.Windows.Forms.Button
+        Me.edtPreview = New System.Windows.Forms.TextBox
+        Me.ListBox1 = New System.Windows.Forms.ListBox
+        Me.lblStatus = New System.Windows.Forms.Label
+        Me.lblReturned = New System.Windows.Forms.Label
+        Me.SuspendLayout()
+        '
+        'edtUsername
+        '
+        Me.edtUsername.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+                    Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.edtUsername.Location = New System.Drawing.Point(68, 38)
+        Me.edtUsername.Name = "edtUsername"
+        Me.edtUsername.Size = New System.Drawing.Size(315, 20)
+        Me.edtUsername.TabIndex = 2
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(9, 38)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(58, 13)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Username:"
+        '
+        'DateTimePicker1
+        '
+        Me.DateTimePicker1.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+                    Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.DateTimePicker1.Location = New System.Drawing.Point(68, 12)
+        Me.DateTimePicker1.Name = "DateTimePicker1"
+        Me.DateTimePicker1.Size = New System.Drawing.Size(315, 20)
+        Me.DateTimePicker1.TabIndex = 4
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(29, 12)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(33, 13)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "Date:"
+        '
+        'btnSearch
+        '
+        Me.btnSearch.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+                    Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnSearch.Location = New System.Drawing.Point(10, 64)
+        Me.btnSearch.Name = "btnSearch"
+        Me.btnSearch.Size = New System.Drawing.Size(373, 41)
+        Me.btnSearch.TabIndex = 9
+        Me.btnSearch.Text = "SEARCH"
+        Me.btnSearch.UseVisualStyleBackColor = True
+        '
+        'edtPreview
+        '
+        Me.edtPreview.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+                    Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.edtPreview.BackColor = System.Drawing.SystemColors.Control
+        Me.edtPreview.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.edtPreview.Location = New System.Drawing.Point(9, 111)
+        Me.edtPreview.Multiline = True
+        Me.edtPreview.Name = "edtPreview"
+        Me.edtPreview.ReadOnly = True
+        Me.edtPreview.Size = New System.Drawing.Size(374, 62)
+        Me.edtPreview.TabIndex = 10
+        '
+        'ListBox1
+        '
+        Me.ListBox1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+                    Or System.Windows.Forms.AnchorStyles.Left) _
+                    Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.ListBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 16
+        Me.ListBox1.Location = New System.Drawing.Point(12, 176)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(371, 340)
+        Me.ListBox1.TabIndex = 11
+        '
+        'lblStatus
+        '
+        Me.lblStatus.Anchor = CType(((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left) _
+                    Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.lblStatus.AutoSize = True
+        Me.lblStatus.Location = New System.Drawing.Point(12, 524)
+        Me.lblStatus.Name = "lblStatus"
+        Me.lblStatus.Size = New System.Drawing.Size(60, 13)
+        Me.lblStatus.TabIndex = 12
+        Me.lblStatus.Text = "Status: Idle"
+        '
+        'lblReturned
+        '
+        Me.lblReturned.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.lblReturned.AutoSize = True
+        Me.lblReturned.Location = New System.Drawing.Point(285, 524)
+        Me.lblReturned.Name = "lblReturned"
+        Me.lblReturned.Size = New System.Drawing.Size(96, 13)
+        Me.lblReturned.TabIndex = 13
+        Me.lblReturned.Text = "Tweets returned: 0"
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(392, 543)
+        Me.Controls.Add(Me.lblReturned)
+        Me.Controls.Add(Me.lblStatus)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.edtPreview)
+        Me.Controls.Add(Me.btnSearch)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.DateTimePicker1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.edtUsername)
+        Me.Name = "frmMain"
+        Me.Text = "TweetADay"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents edtUsername As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents DateTimePicker1 As System.Windows.Forms.DateTimePicker
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents btnSearch As System.Windows.Forms.Button
+    Friend WithEvents edtPreview As System.Windows.Forms.TextBox
+    Friend WithEvents ListBox1 As System.Windows.Forms.ListBox
+    Friend WithEvents lblStatus As System.Windows.Forms.Label
+    Friend WithEvents lblReturned As System.Windows.Forms.Label
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,264 @@
+﻿Imports System
+Imports System.IO
+Imports System.Net
+Imports System.Globalization
+Imports System.Threading
+
+Public Class frmMain
+
+    Dim filetext As String
+
+    'Private Sub loadWebpage(ByVal addy As String)
+    '    Dim p As New Process()
+    '    Dim psi As New ProcessStartInfo(addy)
+    '    p.StartInfo = psi
+    '    p.Start()
+    'End Sub
+
+    Private Sub twitterAPIButton()
+        Dim oFile As System.IO.File
+        Dim oRead As System.IO.StreamReader
+        Dim counter As Integer = 1
+        Dim retrycounter As Integer = 0
+        Dim s As String
+
+
+        ListBox1.Items.Clear()
+
+        While (True)
+
+            filetext = ""
+            Try
+tryagain:
+                If retrycounter = 5 Then
+                    btnSearch.Enabled = True
+                    lblStatus.Text = "Status: Connection error"
+                    Exit Sub
+                End If
+                My.Computer.Network.DownloadFile(buildURL() & counter.ToString, "WineList.txt", "", "", False, 100000000, True)
+                s = buildURL() & counter.ToString
+            Catch
+                retrycounter += 1
+                GoTo tryagain
+            End Try
+
+            oRead = oFile.OpenText("WineList.txt")
+
+            While oRead.Peek <> -1
+                filetext += oRead.ReadLine() + vbNewLine
+            End While
+
+            oRead.Close()
+
+            If filetext.Contains("<entry>") = False Then
+                Exit While
+            End If
+
+            Try
+                filetext = filetext.Remove(0, filetext.IndexOf("<entry>"))
+                pagetolistxml()
+            Catch
+            End Try
+
+            counter += 1
+            System.IO.File.Delete("WineList.txt")
+            ' loadWebpage(buildURL())
+        End While
+
+        If ListBox1.Items.Count = 0 Then
+            ListBox1.Items.Add("No results returned")
+        End If
+
+
+        btnSearch.Enabled = True
+        lblStatus.Text = "Status: Idle"
+        lblReturned.Text = "Tweets returned:" & ListBox1.Items.Count
+    End Sub
+
+    Private Function buildURL()
+        '"http://search.twitter.com/search.atom?q=from%3Arosevibe&until=2009-11-10&since=2009-11-10"
+        Dim url As String = "http://search.twitter.com/search.atom?q=from%3A"
+        url += edtUsername.Text
+        url += "&until=" & DateTimePicker1.Value.ToString("yyyy-MM-dd")
+        url += "&since=" & DateTimePicker1.Value.ToString("yyyy-MM-dd")
+        url += "&page="
+        Return url
+    End Function
+
+    Private Sub htmlButton()
+        Dim oFile As System.IO.File
+        Dim oRead As System.IO.StreamReader
+        Dim counter As Integer
+        Dim found As Boolean
+        Dim retrycounter As Integer = 0
+
+        counter = 1
+        found = False
+        ListBox1.Items.Clear()
+
+        Dim myDTFI As DateTimeFormatInfo = New DateTimeFormatInfo()
+        myDTFI.ShortDatePattern = "h:mm tt MMM d/c/c"
+
+
+        If edtUsername.Text.Trim = "" Then
+            MsgBox("A username must be entered.")
+            Exit Sub
+        End If
+
+        While (True)
+
+            filetext = ""
+
+            Try
+tryagain:
+                If retrycounter = 5 Then
+                    btnSearch.Enabled = True
+                    lblStatus.Text = "Status: Connection error"
+                    Exit Sub
+                End If
+                My.Computer.Network.DownloadFile("http://twitter.com/account/profile.mobile?page=" & counter.ToString & "&user=" & edtUsername.Text, "WineList.txt", "", "", False, 100000000, True)
+            Catch
+                retrycounter += 1
+                GoTo tryagain
+            End Try
+
+            oRead = oFile.OpenText("WineList.txt")
+
+            While oRead.Peek <> -1
+                filetext += oRead.ReadLine() + vbNewLine
+            End While
+
+            oRead.Close()
+
+            filetext = filetext.Remove(0, filetext.IndexOf("<div class=" & Chr(34) & "s" & Chr(34) & "><b>Previous Tweets</b></div>") + 62)
+            Try
+                filetext = filetext.Substring(0, filetext.IndexOf("</ul>"))
+            Catch
+                Exit While
+            End Try
+
+            If Not (filetext.Substring(filetext.IndexOf("<small>") + Len("<small>"), filetext.IndexOf("</small>") - filetext.IndexOf("<small>") - Len("<small>")).Contains("ago")) Then
+                If DateTime.Parse(filetext.Substring(filetext.IndexOf("<small>") + Len("<small>"), filetext.IndexOf("</small>") - filetext.IndexOf("<small>") - Len("<small>")).Replace("rd", "").Replace("th", "").Replace("st", "").Replace("nd", ""), myDTFI) < DateTimePicker1.Value.Date Then
+                    Exit While
+                Else
+                    lblStatus.Text = "Status: Searching... (" & DateTime.Parse(filetext.Substring(filetext.IndexOf("<small>") + Len("<small>"), filetext.IndexOf("</small>") - filetext.IndexOf("<small>") - Len("<small>")).Replace("rd", "").Replace("th", "").Replace("st", "").Replace("nd", ""), myDTFI).ToString("MMMM") & ") Tweets scanned: " & counter * 20
+                End If
+            End If
+
+            If filetext.Contains(DateTimePicker1.Value.ToString("MMM d")) Then
+                If (DateTimePicker1.Value.Year < Today.Year And filetext.Contains(DateTimePicker1.Value.Year.ToString & "</small>")) Or DateTimePicker1.Value.Year = Now().Year Then
+                    found = True
+                    pagetolisthtml()
+                End If
+            End If
+
+            counter = counter + 1
+
+            If found = True And (filetext.Contains(DateTimePicker1.Value.ToString("MMM d"))) = False Then
+                Exit While
+            End If
+
+        End While
+
+        If ListBox1.Items.Count = 0 Then
+            ListBox1.Items.Add("No results returned")
+        End If
+
+        btnSearch.Enabled = True
+        lblStatus.Text = "Status: Idle"
+        System.IO.File.Delete("WineList.txt")
+
+        lblReturned.Text = "Tweets returned:" & ListBox1.Items.Count
+
+
+
+    End Sub
+
+    Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnSearch.Click
+        If DateTimePicker1.Value.Date > Today Then
+            MsgBox("Search cannot be in the future")
+            Exit Sub
+        End If
+
+        If edtUsername.Text.Trim = "" Then
+            MsgBox("A username must be entered.")
+            Exit Sub
+        End If
+
+
+        If DateDiff(DateInterval.Day, DateTimePicker1.Value.Date, Today) > 7 Then
+            lblStatus.Text = "Status: Searching..."
+            btnSearch.Enabled = False
+            Dim trd As Thread = New Thread(AddressOf htmlButton)
+            trd.IsBackground = True
+            trd.Start()
+            'htmlButton()
+        Else
+            lblStatus.Text = "Status: Searching..."
+            btnSearch.Enabled = False
+            Dim trd As Thread = New Thread(AddressOf twitterAPIButton)
+            trd.IsBackground = True
+            trd.Start()
+            'twitterAPIButton()
+        End If
+
+        edtPreview.Text = ""
+        ListBox1.SelectedIndex = -1
+    End Sub
+
+
+    Private Sub pagetolisthtml()
+        Dim templist As String
+        Dim tempstr As String
+
+        tempstr = filetext
+
+        While tempstr.Contains("<li>")
+            tempstr = tempstr.Trim()
+            templist = tempstr.Substring(4, tempstr.IndexOf("</li>") - 4)
+            If templist.Contains(DateTimePicker1.Value.ToString("MMM d")) Then
+                templist = fixlistitem(templist)
+                ListBox1.Items.Add(templist)
+            End If
+            tempstr = tempstr.Remove(0, tempstr.IndexOf("</li>") + 5)
+        End While
+    End Sub
+
+    Private Sub pagetolistxml()
+        Dim templist As String
+        Dim tempstr As String
+
+        tempstr = filetext
+
+
+
+        While tempstr.Contains("<entry>")
+            tempstr = tempstr.Trim()
+            templist = tempstr.Substring((tempstr.IndexOf("<title>") + Len("<title>")), tempstr.IndexOf("</title>") - (tempstr.IndexOf("<title>") + Len("<title>")))
+
+            ListBox1.Items.Add(templist)
+            tempstr = tempstr.Remove(0, tempstr.IndexOf("</entry>") + Len("</entry>"))
+        End While
+    End Sub
+
+
+    Private Function fixlistitem(ByVal item As String)
+        item = item.Substring(0, item.IndexOf("<small>"))
+
+        While item.IndexOf("<") > -1 And item.IndexOf(">") > -1
+            item = item.Remove(item.IndexOf("<"), item.IndexOf(">") - item.IndexOf("<") + 1)
+        End While
+
+        Return item
+    End Function
+
+    Private Sub ListBox1_SelectedIndexChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ListBox1.SelectedIndexChanged
+        edtPreview.Text = ListBox1.SelectedItem
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Control.CheckForIllegalCrossThreadCalls = False
+    End Sub
+
+
+End Class
